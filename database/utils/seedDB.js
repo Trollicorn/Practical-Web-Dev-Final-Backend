0c58651d7a7c7ec3d2c375f8043b02a4e4e54ab3
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2022-05-24 16:57:14 -0400

    added more examples for testing

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -10,15 +10,42 @@ const seedDB = async () => {
 		firstname: "Kim",
 		lastname: "Kardashian"
 	});
+	const dummyInstructor3 = await Instructor.create({
+		firstname: "John",
+		lastname: "Doe",
+		department: "Hackery"
+	});
+	const dummyInstructor4 = await Instructor.create({
+		firstname: "Pikachu",
+		lastname: "Of Doom",
+		department: "Pokemon",
+		imageUrl: "https://archives.bulbagarden.net/media/upload/thumb/0/0d/025Pikachu.png/240px-025Pikachu.png"
+	});
 
 	const dummyCourse = await Course.create({
 		title: "CSCI 39548",
         location: "C107",
         timeslot: "W 5:35 - 8:15 PM"
 	});
+	const dummyCourse2 = await Course.create({
+		title: "Lunch",
+        location: "Cafeteria",
+        timeslot: "Everyday"
+	});
+	const dummyCourse3 = await Course.create({
+		title: "Suffering",
+        location: "Everywhere",
+        timeslot: "Always"
+	});
+	const dummyCourse4 = await Course.create({
+		title: "CSCI 49900",
+        location: "C107",
+        timeslot: "MW 3:10 - 5:25 PM"
+	});
 
 	await dummyCourse.setInstructor(dummyInstructor);
-	
+	await dummyCourse2.setInstructor(dummyInstructor4);
+	await dummyCourse3.setInstructor(dummyInstructor4);
 }
 
 module.exports = seedDB;
